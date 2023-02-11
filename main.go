1716ecfdf2b462cdd19f94611414000fc9f25252
Sandhu-Sahil <sandhu.sahil2002@gmail.com>
2023-02-11 21:40:02 +0530

    Learning Git and Mongodb integrated

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sandhu-sahil/gin-and-mongodb/controllers"
+	"sandhu-sahil/gin-and-mongodb/services"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	server      *gin.Engine
+	us          services.UserService
+	uc          controllers.UserController
+	ctx         context.Context
+	userc       *mongo.Collection
+	mongoclient *mongo.Client
+	err         error
+)
+
+func init() {
+	ctx = context.TODO()
+
+	mongoconn := options.Client().ApplyURI("mongodb+srv://Sandhu-Sahil:ssssandhu26@cluster0.yfxftsl.mongodb.net/?retryWrites=true&w=majority")
+	mongoclient, err = mongo.Connect(ctx, mongoconn)
+	if err != nil {
+		log.Fatal("error while connecting with mongo", err)
+	}
+	err = mongoclient.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal("error while trying to ping mongo", err)
+	}
+
+	fmt.Println("mongo connection established")
+
+	userc = mongoclient.Database("gin-golang").Collection("users")
+	us = services.NewUserService(userc, ctx)
+	uc = controllers.New(us)
+	server = gin.Default()
+}
+
+func main() {
+	defer mongoclient.Disconnect(ctx)
+
+	basepath := server.Group("/v1")
+	uc.RegisterUserRoutes(basepath)
+
+	log.Fatal(server.Run(":9090"))
+
+}
